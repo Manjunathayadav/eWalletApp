9a43e11685ac3f566bfce52dc83e7d0ebd9d3a00
rupinder-tech <55372758+rupinder-tech@users.noreply.github.com>
2026-01-31 16:22:16 +0530

    Update GlobalExceptionHandler.java

--- WalletService/WalletService/src/main/java/com/WalletService/exception/GlobalExceptionHandler.java
+++ WalletService/WalletService/src/main/java/com/WalletService/exception/GlobalExceptionHandler.java
@@ -1,10 +1,27 @@
-package com.hcl.PaymentService;
+package com.walletService.exception;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-public class WalletService {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-	
+    @ExceptionHandler(WalletNotFoundException.class)
+    public ResponseEntity<String> handleWalletNotFoundException(
+            WalletNotFoundException ex) {
 
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    // Optional: generic fallback
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong");
+    }
 }
