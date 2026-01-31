483b773ba05b81962ba7258584f813399e902d1c
rupinder-tech <55372758+rupinder-tech@users.noreply.github.com>
2026-01-31 17:09:07 +0530

    Update CreateWalletRequest.java

--- WalletService/WalletService/src/main/java/com/WalletService/dto/CreateWalletRequest.java
+++ WalletService/WalletService/src/main/java/com/WalletService/dto/CreateWalletRequest.java
@@ -11,6 +11,46 @@ public class CreateWalletRequest {
 	private String walletId;
 	private String customerId;
 
+	public String getCurrency() {
+    return currency;
+}
+
+public void setCurrency(String currency) {
+    this.currency = currency;
+}
+
+public String getStatus() {
+    return status;
+}
+
+public void setStatus(String status) {
+    this.status = status;
+}
+
+public Long getBalance() {
+    return balance;
+}
+
+public void setBalance(Long balance) {
+    this.balance = balance;
+}
+
+public String getWalletId() {
+    return walletId;
+}
+
+public void setWalletId(String walletId) {
+    this.walletId = walletId;
+}
+
+public String getCustomerId() {
+    return customerId;
+}
+
+public void setCustomerId(String customerId) {
+    this.customerId = customerId;
+}
+
 	
 
 }
