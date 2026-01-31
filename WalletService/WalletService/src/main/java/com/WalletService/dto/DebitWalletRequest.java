19df4a6608e04f92c883efdbc7ceaf98a7fa11e2
rupinder-tech <55372758+rupinder-tech@users.noreply.github.com>
2026-01-31 17:00:54 +0530

    Update DebitWalletRequest.java

--- WalletService/WalletService/src/main/java/com/WalletService/dto/DebitWalletRequest.java
+++ WalletService/WalletService/src/main/java/com/WalletService/dto/DebitWalletRequest.java
@@ -3,7 +3,7 @@ package com.hcl.PaymentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class WalletService {
+public class DebitWalletRequest {
 
 	
 
